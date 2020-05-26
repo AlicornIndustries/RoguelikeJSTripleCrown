031fab98259f43055620eb69d172f939e7785668
AlicornIndustries <{ID}+{username}@users.noreply.github.com>
2020-05-26 14:11:38 -0700

    About to strip it all out

--- game.js
+++ game.js
@@ -10,10 +10,10 @@ var Game = {
         this._createPlayer();
         this.player._draw();
 
-        // var scheduler = new ROT.Scheduler.Simple();
-        // scheduler.add(this.player, true);
-        // this.engine = new ROT.Engine(scheduler);
-        // this.engine.start();
+        var scheduler = new ROT.Scheduler.Simple();
+        scheduler.add(this.player, true);
+        this.engine = new ROT.Engine(scheduler);
+        this.engine.start();
     },
     _createDisplay: function() {
         this.display = new ROT.Display(this.displayOptions);
@@ -40,24 +40,35 @@ var Player = function(x,y) {
     this._draw();
 }
 
-// TODO: Figure out better why we need to use prototypes instead of defining this in var Player
-// Player.prototype.act = function(e) {
-//     // Process user input. Called on keydown by Player.prototype.act()'s event listener
-//     // TODO: make keymapping configurable. Put it in a separate var?
-//     var keyMap = {};
-//     // Numpad keys (Top is 0 (aka the 8 key), going clockwise)
-//     keyMap[38] = 0;keyMap[33] = 1;keyMap[39] = 2;keyMap[34] = 3;keyMap[40] = 4;keyMap[35] = 5;keyMap[37] = 6;keyMap[36] = 7;
+Player.prototype.act = function() {
+    Game.engine.lock(); // Wait for user input
+    window.addEventListener("keydown", this);
+}
 
-//     var code = e.keyCode; // button pressed by user
+Player.prototype.handleEvent = function(e) {
+    // Process user input. Called on keydown by Player.prototype.act()'s event listener
+    // TODO: make keymapping configurable. Put it in a separate var?
+    var keyMap = {};
+    // Numpad keys (Top is 0 (aka the 8 key), going clockwise)
+    // TODO: Replace with proper keycodes: https://ondras.github.io/rot.js/doc/modules/_constants_.html#keys
+    keyMap[38] = 0;keyMap[33] = 1;keyMap[39] = 2;keyMap[34] = 3;keyMap[40] = 4;keyMap[35] = 5;keyMap[37] = 6;keyMap[36] = 7;
 
-//     // Validate input: is the key code mapped?
-//     if(!(code in keyMap)) {return;}
-//     // Now check if player can move in that direction. TODO: replace with canMoveInDirection() function of Player
-//     var diff = ROT.DIRS[8][keyMap[code]]; // x,y dirs of the move (e.g. 6 on the numpad is 1,0)
-//     var newX = this._x + diff[0];
-//     var newY = this._y + diff[1];
-// }
+    var code = e.keyCode; // button pressed by user
 
+    // Validate input: is the key code mapped?
+    if(!(code in keyMap)) {return;}
+    // Now check if player can move in that direction. TODO: replace with canMoveInDirection() function of Player
+    var delta = ROT.DIRS[8][keyMap[code]]; // x,y dirs of the move (e.g. 6 on the numpad is 1,0)
+    var newX = this._x + delta[0];
+    var newY = this._y + delta[1];
+    // TODO: Figure out how to determine if a tile is navigable.
+}
+
+Player.prototype.canMoveInDirection = function(deltaX,deltaY) {
+    
+}
+
+// Move this into a higher-level Actor class?
 Player.prototype._draw = function() {
     // Call this after moving
     Game.display.draw(this._x, this._y, "@", "#FFFFFF");
